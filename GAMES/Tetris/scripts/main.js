0d99e6b0afa77bd258573315835f6ca56e619021
YordanPetkov <yordan_petkov180@abv.bg>
2019-04-16 20:58:18 +0300

    Seperate draw file.

--- GAMES/Tetris/scripts/main.js
+++ GAMES/Tetris/scripts/main.js
@@ -1,18 +1,15 @@
     "use strict"
-    const canvas = document.getElementById("tetris-canvas");
-    const context = canvas.getContext("2d");
-    const requestAnimationFrame = (window.requestAnimationFrame ||
-            window.mozRequestAnimationFrame ||
-            window.webkitRequestAnimationFrame ||
-            function(callback) {
-                setTimeout(callback, 1000 / 30); // 30 FPS
-            });
+    
+    
     const TETRIS_ROWS = 18;
     const TETRIS_COLS = 10;
     const TETRIS_CELL_SIZE = 20;
 
-    canvas.width = 800;
-    canvas.height = 600;
+    const tetrisTable = Array.from({length: TETRIS_ROWS})
+            .map(() => Array.from({ length: TETRIS_COLS})
+            .map(() => false));
+
+    
 
     let x = 10,
         y = 10;
@@ -22,29 +19,7 @@
         x += 1;
     }
 
-    function drawGrid(){
-        for(let i = 0; i <= TETRIS_ROWS; i += 1){
-            context.moveTo(0, TETRIS_CELL_SIZE * i);
-            context.lineTo(TETRIS_COLS * TETRIS_CELL_SIZE, TETRIS_CELL_SIZE * i);
-            context.stroke();
-        }
-
-        for(let i = 0; i <= TETRIS_COLS; i += 1){
-            context.moveTo(TETRIS_CELL_SIZE * i, 0);
-            context.lineTo(TETRIS_CELL_SIZE * i, TETRIS_ROWS * TETRIS_CELL_SIZE);
-            context.stroke();
-        }
-    }
-    function draw(){
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        
-        drawGrid();
-
-        requestAnimationFrame(draw);
-    }
-
-
-    draw();
+    
     setInterval(update,100);
 
 
