384ebf0b5d7afcfdd0b04e0d0535ae3261d57d5b
YordanPetkov <yordan_petkov180@abv.bg>
2019-04-21 22:04:40 +0300

    Added reflect.

--- GAMES/Multiplayer Games/reflect/game.js
+++ GAMES/Multiplayer Games/reflect/game.js
@@ -1,23 +1,52 @@
-// Creating variables
-var socket = io();
+function orient(x1, y1, x2, y2, x3, y3){
+    return x2*y3 + x1*y2 + y1*x3 - y1*x2 - y2*x3 - y3*x1;
+}
 
-socket.on("number", function(numberOfClients) {
-    console.log(numberOfClients);
-});
+
+function areIntersecting(x1, y1, x2, y2, x3, y3, x4, y4){
+    let o1 = orient(x1, y1, x3, y3, x4, y4);
+    let o2 = orient(x2, y2, x3, y3, x4, y4);
+    let o3 = orient(x3, y3, x1, y1, x2, y2);
+    let o4 = orient(x4, y4, x1, y1, x2, y2);
+    if (o1==0 || o2==0 || o3==0 || o4==0){return 0;}
+    return ((o1>0)^(o2>0)) && ((o3>0)^(o4>0));
+}
+
+// Creating variables
+//var socket = io();
+var myX = 400, myY = 300, angle = 0, speed = 0;
+var bx = 100, by = 100, ex= 600, ey=300;
 
 function update() {
-    
+    let dX = Math.cos(angle)*speed;
+    let dY = Math.sin(angle)*speed;
+
+    if(areIntersecting(myX,myY,myX+dX,myY+dY,bx,by,ex,ey)){
+        angle = -angle + 2*Math.atan2(ey-by, ex-bx);
+    }
+    else{
+        myX += dX;
+        myY += dY;
+    }
+   
 }
 
 function draw() {
-    
+    context.fillRect(myX, myY, 10, 10);
+
+    context.beginPath();
+    context.moveTo(bx, by);
+    context.lineTo(ex, ey);
+    context.stroke();
 }
 
 function keyup(key) {
-	if(key == 32){
-        socket.emit("space");
-    }
+
 }
+
 function mouseup() {
-    
+    angle = Math.atan2(mouseY - myY, mouseX - myX);
+    speed = 5;
+
+    console.log("Mouse clicked at",mouseX,mouseY);
 }
