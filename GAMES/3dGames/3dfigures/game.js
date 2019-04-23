56f4768ba13e23601e2fdb92e735a5bdf8bd5644
YordanPetkov <yordan_petkov180@abv.bg>
2019-04-23 18:18:17 +0300

    Added cube grid.

--- GAMES/3dGames/3dfigures/game.js
+++ GAMES/3dGames/3dfigures/game.js
@@ -1,20 +1,31 @@
 // Creating variables
-var geometry = new THREE.BoxGeometry( 2, 2, 2 );
+var geometry = new THREE.BoxGeometry( 0.9, 0.9, 0.9 );
 var material = new THREE.MeshPhongMaterial();
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+
+for(let i = -5; i < 5; i++){
+	for(let j = -5; j < 5; j++){
+		var cube = new THREE.Mesh( geometry, material );
+		cube.position.set(i, 0, j);
+		scene.add( cube );
+	}
+	
+}
+
+camera.position.set(7, 10, 16);
+camera.lookAt(new THREE.Vector3(0,0,0));
+
 
 var light = new THREE.PointLight( );
 var light2 = new THREE.PointLight( );
-light.position.set(2,2,3);
-light2.position.set(-2, -2, 3);
+light.position.set(8, 8, 12);
+light2.position.set(-8, -8, 12);
 scene.add( light );
 scene.add( light2 );
 
 function update() {
-	cube.rotation.x += 0.015;
+	/* cube.rotation.x += 0.015;
 	cube.rotation.y += 0.010;
-	cube.rotation.z += 0.005;
+	cube.rotation.z += 0.005; */
 }
 
 function keyup(key) {
