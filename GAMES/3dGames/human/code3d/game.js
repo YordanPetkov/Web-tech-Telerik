a2c2c00424aca6273429719ea6ba2256b6fc919a
YordanPetkov <yordan_petkov180@abv.bg>
2019-04-23 19:25:45 +0300

    Added movement with keyboard.

--- GAMES/3dGames/human/code3d/game.js
+++ GAMES/3dGames/human/code3d/game.js
@@ -20,33 +20,57 @@ scene.add(right_arm);
 var leg_geometry = new THREE.BoxGeometry(0.8, 4, 1);
 var left_leg = new THREE.Mesh( leg_geometry, material);
 var right_leg = new THREE.Mesh( leg_geometry, material);
-left_leg.position.set( 0.6, -3.5, 0);
-right_leg.position.set( -0.6, -3.5, 0);
+left_leg.position.set( 0.5, -3.5, 0);
+right_leg.position.set( -0.5, -3.5, 0);
 scene.add(left_leg);
 scene.add(right_leg);
 
-camera.position.set(12, 8, 16);
+camera.position.set(25, 18, 35);
 camera.lookAt(new THREE.Vector3(0, 0, 0));
 
 var light = new THREE.PointLight( );
 var light2 = new THREE.PointLight( );
 var light3 = new THREE.PointLight( );
-light.position.set(-10,10,10);
-light2.position.set(10, 10, -5);
-light3.position.set(0, -10 , 5);
+light.position.set(-100,100,100);
+light2.position.set(100, 100, -50);
+light3.position.set(0, -100 , 50);
 scene.add( light );
 scene.add( light2 );
 scene.add( light3 );
+
+var parts = [cube, head, left_arm, right_arm, left_leg, right_leg];
 
 function update() {
 	/* cube.rotation.x += 0.015;
 	cube.rotation.y += 0.010;
 	cube.rotation.z += 0.005; */
+	
+	var delta = 0.01;
+	for(let i = 0; i<parts.length; i++){
+		if(isKeyPressed[87]){
+			parts[i].position.z +=delta;
+		}	
+		if(isKeyPressed[83]){
+			parts[i].position.z -=delta;
+		}	
+		if(isKeyPressed[65]){
+			parts[i].position.x +=delta;
+		}	
+		if(isKeyPressed[68]){
+			parts[i].position.x -=delta;
+		}	
+	}
+	
 }
 
 function keyup(key) {
 	// Show the pressed keycode in the console
-	console.log("Pressed", key);
+	console.log("Keyup", key);
+}
+
+function keydown(key) {
+	// Show the pressed keycode in the console
+	console.log("Keydown", key);
 }
 function mouseup() {
 	// Show coordinates of mouse on click
