67884bbea744aa7a79cc3a68c1461deccedfeae7
YordanPetkov <yordan_petkov180@abv.bg>
2019-04-25 15:25:53 +0300

    Added unity demo.

---
+++ Unity2D/Basics/demo1/Assets/scripts/MovementControler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementControler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	public float speed = 0.05f;
+	// Update is called once per frame
+	void Update () {
+		var position = this.transform.position;
+		position.x += speed;
+		this.transform.position = position;
+	}
+}
