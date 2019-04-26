11ea4aaf82a5c7566c9743eabc69b73f995315d7
YordanPetkov <yordan_petkov180@abv.bg>
2019-04-26 18:38:33 +0300

    Added background movement.

---
+++ Unity2D/FlappyBird/FlappyBird/Assets/scripts/BackgroundCollideControler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+
+public class BackgroundCollideControler : MonoBehaviour
+{
+
+    private int numberOfBackgrounds;
+    private float distanceBetweenBackgrounds;
+
+    private int numberOfGrounds;
+    private float distanceBetweenGrounds;
+
+    public void Start()
+    {
+        var backgrounds = GameObject.FindGameObjectsWithTag("Background");
+        var grounds = GameObject.FindGameObjectsWithTag("Ground");
+        this.numberOfBackgrounds = backgrounds.Length;
+        this.numberOfGrounds = grounds.Length;
+
+        if (this.numberOfBackgrounds < 2 || this.numberOfGrounds < 2)
+        {
+            throw new InvalidOperationException("You must have at least two backgrounds or grounds in your scene!");
+        }
+
+        this.distanceBetweenBackgrounds
+            = Math.Abs(backgrounds[1].transform.position.x
+            - backgrounds[0].transform.position.x);
+        this.distanceBetweenGrounds
+            = Math.Abs(grounds[1].transform.position.x
+            - grounds[0].transform.position.x);
+    }
+
+    public void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.CompareTag("Background"))
+        {
+            var background = collider.gameObject;
+            var bgPosition = background.transform.position;
+            bgPosition.x += this.numberOfBackgrounds * this.distanceBetweenBackgrounds;
+            background.transform.position = bgPosition;
+        }
+        if (collider.CompareTag("Ground"))
+        {
+            var ground = collider.gameObject;
+            var gPosition = ground.transform.position;
+            gPosition.x += this.numberOfGrounds * this.distanceBetweenGrounds;
+            ground.transform.position = gPosition;
+        }
+    }
+}
